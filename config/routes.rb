e543802a01083e4888d1081d914e39ea70bfb003
christina <christinasirena@gmail.com>
2022-04-27 16:14:52 -0700

    technician controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'technician/index'
+  get 'technician/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #displays scheduling grin in user/index.html.erb
   root "users#index"
