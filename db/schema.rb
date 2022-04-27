137852eb96aa40446a07c0b2b98d1ae26c94c7f9
christina <christinasirena@gmail.com>
2022-04-27 16:50:41 -0700

    add location controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_215033) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_27_233131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "technicians", force: :cascade do |t|
+    t.string "first_name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "title"
