83b97e382cd87d99dba8224c715a952b58fb7584
christina <christinasirena@gmail.com>
2022-04-27 20:48:06 -0700

    add workorder controller

---
+++ app/controllers/work_orders_controller.rb
@@ -0,0 +1,7 @@
+class WorkOrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ test/controllers/work_orders_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class WorkOrdersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get work_orders_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get work_orders_show_url
+    assert_response :success
+  end
+end
